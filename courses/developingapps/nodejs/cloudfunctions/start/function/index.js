f700cc6c77980f73991d1849974e3d5a01a3f38d
benk210 <36677579+benk210@users.noreply.github.com>
2018-02-20 14:20:49 -0600

    Update index.js

--- courses/developingapps/nodejs/cloudfunctions/start/function/index.js
+++ courses/developingapps/nodejs/cloudfunctions/start/function/index.js
@@ -20,7 +20,7 @@ const languageAPI = require('./languageapi');
 
 // TODO: Load the ./spanner module
 
-
+const feedbackStorage = require('./spanner');
 
 // END TODO
 
@@ -33,7 +33,8 @@ exports.subscribe = function subscribe(event) {
   // extracting the feedbackObject data
   // The message received from Pub/Sub is base64 encoded, and 
   // the data submitted by students is in a data property
-
+ let feedbackObject = JSON.parse(Buffer.from(
+        pubsubMessage.data, 'base64').toString()).data;
 
 
   // END TODO
@@ -42,36 +43,37 @@ exports.subscribe = function subscribe(event) {
   // The analyze(...) method expects to be passed the 
   // feedback text from the feedbackObject as an argument, 
   // and returns a Promise.
+  return languageAPI.analyze(feedbackObject.feedback)
+    .then(
+      score => {
+        // TODO: Log the sentiment score
+             
+        console.log(`Score: ${score}`);
 
+        // END TODO
 
+        // TODO: Add new score property to feedbackObject
 
-  // TODO: Log the sentiment score
+             feedbackObject.score = score;
 
+        // END TODO
 
+        // TODO: Pass feedback object to the next handler
 
-  // END TODO
+             return feedbackObject;
 
-  // TODO: Add new score property to feedbackObject
+        // END TODO
+      }) // TODO: insert record
+    .then(feedbackStorage.saveFeedback) 
+    .then(() => {
+       // TODO: Log and return success
 
-
-
-  // END TODO
-
-  // TODO: Pass feedback object to the next handler
-
-
-
-  // END TODO
-  // TODO: insert record
-
-  // TODO: Log and return success
-
-
-
-  // END TODO
-
-  // TODO: Log error
-
+           console.log('feedback saved...');
+          return 'success';  
+  
+       // END TODO
+        
+    }).catch(console.error); // TODO: Log error
 };
 
 
